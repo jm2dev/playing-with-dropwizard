495b309d13fb6941540b629c202985d4c4f58f1f
jm2dev <jomarcar@gmail.com>
2014-07-22 02:14:14 +0100

    Refactoring xsl transformation test.

--- csv-xslt/src/main/resources/xslt/xml2csv.xsl
+++ csv-xslt/src/main/resources/xslt/xml2csv.xsl
@@ -1,12 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
 <xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:output method="text" indent="no" encoding="utf-8"/>
+    <xsl:output method="text" indent="no" encoding="utf-8"/>
+    <xsl:strip-space elements="*"/>
     <xsl:template match="/">
         date,description,amount,balance
-        <xsl:apply-templates />
+        <xsl:apply-templates/>
     </xsl:template>
 
     <xsl:template match="/statements/statement">
-        <xsl:value-of select="date"/>,<xsl:value-of select="description"/>,<xsl:value-of select="amount"/>,<xsl:value-of select="balance"/>
+        <xsl:value-of select="date"/>,<xsl:value-of select="description"/>,<xsl:value-of select="amount"/>,<xsl:value-of
+            select="balance"/>
     </xsl:template>
 </xsl:stylesheet>
--- csv-xslt/src/test/java/com/jm2dev/xslt/TransformationsTest.java
+++ csv-xslt/src/test/java/com/jm2dev/xslt/TransformationsTest.java
@@ -17,24 +17,28 @@ public class TransformationsTest {
     @Test
     public void transformXmlStatementIntoCsv() {
         try {
-            Processor processor = new Processor(false);
-            XsltCompiler compiler = processor.newXsltCompiler();
-            XsltExecutable executable = compiler.compile(new StreamSource(new File("src/main/resources/xslt/xml2csv.xsl")));
-            XdmNode source = processor.newDocumentBuilder().build(new File("src/test/resources/bank.xml"));
-            Serializer out = processor.newSerializer(new File("src/test/resources/bank.csv"));
-            out.setOutputProperty(Serializer.Property.METHOD, "text");
-            out.setOutputProperty(Serializer.Property.INDENT, "no");
-            XsltTransformer transformer = executable.load();
-            transformer.setInitialContextNode(source);
-            transformer.setDestination(out);
-            transformer.transform();
-
-            Path actual = new File("src/test/resources/bank.csv").toPath();
-            assertThat(Files.readAllLines(actual), IsCollectionContaining.hasItem("    dd-mm-yyyy,this is a description,100.00,1000.00"));
+            String xsl = "src/main/resources/xslt/xml2csv.xsl";
+            String input = "src/test/resources/bank.xml";
+            String output = "src/test/resources/bank.csv";
+            performTransformation(xsl, input, output);
+            Path actual = new File(output).toPath();
+            assertThat(Files.readAllLines(actual), IsCollectionContaining.hasItem("        dd-mm-yyyy,this is a description,100.00,1000.00"));
         } catch (SaxonApiException e) {
             assertTrue(false);
         } catch (IOException ioe) {
             assertTrue(false);
         }
+    }
+
+    private void performTransformation(String xsl, String input, String output) throws SaxonApiException {
+        Processor processor = new Processor(false);
+        XsltCompiler compiler = processor.newXsltCompiler();
+        XsltExecutable executable = compiler.compile(new StreamSource(new File(xsl)));
+        XdmNode source = processor.newDocumentBuilder().build(new File(input));
+        Serializer out = processor.newSerializer(new File(output));
+        XsltTransformer transformer = executable.load();
+        transformer.setInitialContextNode(source);
+        transformer.setDestination(out);
+        transformer.transform();
     }
 }
